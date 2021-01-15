ba024af33c302117aea4df8b87d967fd2e8ec49d
vkowalski <vkowalski@lab-code.com>
2021-01-15 18:18:01 +0300

    counter

--- app/src/main/java/com/victor/ko/scart/activities/AuthActivity.kt
+++ app/src/main/java/com/victor/ko/scart/activities/AuthActivity.kt
@@ -1,6 +1,7 @@
 package com.victor.ko.scart.activities
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -38,8 +39,8 @@ class AuthActivity : AppCompatActivity() {
     private fun requirePin() {
         bnd.continueButton.visibility = View.GONE
         bnd.sendAgainButton.isEnabled = false
-        //countDownTimer.cancel()
-        //countDownTimer.start()
+        countDownTimer.cancel()
+        countDownTimer.start()
 
 //***************
         CoroutineScope(Dispatchers.Main).launch() {
@@ -50,10 +51,26 @@ class AuthActivity : AppCompatActivity() {
                 if (response.isSuccessful && response.body() != null) {
                     // Retrieve data.
                     val data = response.body()!!
+                    print(data)
                     /*data.message?.let {
                         // Load URL into the ImageView using Coil.
                         //iv_dog_image.load(it)
                     }*/
+
+                    when {
+                        data.success != null -> {
+                            //Paper.book().write("authToken", pin.success.token)
+                            bnd.sendCodeForm?.visibility = View.VISIBLE
+                            if (BuildConfig.DEBUG) {
+                                bnd.pinTextEdit.setText(data.success.pin)
+                            }
+                        }
+                        data.error != null -> {
+                            bnd.pinErrorLabel.text = data.error.message
+                        }
+                    }
+
+
                 } else {
                     // Show API error.
                     // This is when the server responded with an error.
@@ -67,11 +84,31 @@ class AuthActivity : AppCompatActivity() {
                 // Show API error. This is the error raised by the client.
                 // The API probably wasn't called in this case, so better check before assuming.
 
-            /*Toast.makeText(this@MainActivity,
-                    "Error Occurred: ${e.message}",
-                    Toast.LENGTH_LONG).show()*/
+                /*Toast.makeText(this@MainActivity,
+                        "Error Occurred: ${e.message}",
+                        Toast.LENGTH_LONG).show()*/
             }
         }
         //**********************
+    }
+
+    private val countDownTimer = object : CountDownTimer(10000, 1000) {
+        override fun onFinish() {
+            runOnUiThread {
+                bnd.sendAgainButton.isEnabled = true
+                bnd.resendVia.text = ""
+            }
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+            runOnUiThread {
+                val min = Math.ceil((millisUntilFinished / 60000).toDouble())
+                val sec = Math.ceil(((millisUntilFinished / 1000) % 60).toDouble())
+
+                val format = resources.getString(R.string.resend_via)
+                bnd.resendVia.text = String.format(format, min, sec)
+
+            }
+        }
     }
 }
